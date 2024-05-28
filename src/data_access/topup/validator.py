bcfb8f347245078ee5683e0a1ded5415de3afef5
Ryder <pandeyabhishek234@gmail.com>
2024-05-29 00:50:45 +0530

    Email content change on verify email

--- src/data_access/topup/validator.py
+++ src/data_access/topup/validator.py
@@ -6,3 +6,9 @@ def request_for_validator(user_id: str, package_id: int, wallet_id: int):
     res = execute_query("call usp_request_for_validator(_user_id => %s, _package_id => %s, _wallet_id => %s)",
                         (user_id, package_id, wallet_id))
     return res
+
+
+def vote_for_validator(validator_user_id: str, by_user_id: str):
+    res = execute_query("call usp_vote_for_validator(_validator_user_id => %s, _by_user_id => %s)",
+                        (validator_user_id, by_user_id))
+    return res
--- src/routers/topup/validator.py
+++ src/routers/topup/validator.py
@@ -28,3 +28,23 @@ async def request_for_validator(package_id: int, wallet_id: int, token_payload: 
     except Exception as e:
         print(e.__str__())
         return {'success': False, 'message': get_error_message(e)}
+
+
+@router.get('/vote_for_validator', dependencies=[Depends(RightsChecker([245]))])
+async def vote_for_validator(validator_user_id: str, token_payload: any = Depends(get_current_user)):
+    try:
+        dataset = data_access.vote_for_validator(validator_user_id=validator_user_id, by_user_id=token_payload["user_id"])
+        # print(dataset)
+        if len(dataset) > 0 and len(dataset['rs']):
+            ds = dataset['rs']
+            if ds.iloc[0].loc["success"]:
+
+                return {'success': True, 'message': ds.iloc[0].loc["message"]}
+
+            return {'success': False, 'message': ds.iloc[0].loc["message"]}
+
+        return {'success': False, 'message': DATABASE_CONNECTION_ERROR}
+
+    except Exception as e:
+        print(e.__str__())
+        return {'success': False, 'message': get_error_message(e)}
