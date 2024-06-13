e25caefcf9641d3085318ccef19b5334905da0b2
Ryder <pandeyabhishek234@gmail.com>
2024-06-14 03:00:37 +0530

    Bug fix

--- src/routers/automation/__init__.py
+++ src/routers/automation/__init__.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter
 from .currency import router as currency_router
 from .arbitrage_trade_transactions import router as arbitrage_trade_transactions_router
+from .validator_transactions import router as validator_transactions_router
 
 
 router = APIRouter(
@@ -11,3 +12,4 @@ router = APIRouter(
 
 router.include_router(currency_router)
 router.include_router(arbitrage_trade_transactions_router)
+router.include_router(validator_transactions_router)
